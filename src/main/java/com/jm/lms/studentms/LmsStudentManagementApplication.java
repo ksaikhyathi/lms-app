5c5eccb27f524ed0de42c11a8e68f5a1bceff26f
Murugeshan <murutech786@gmail.com>
2024-12-17 07:47:59 +0530

    Add main class

---
+++ src/main/java/com/jm/lms/studentms/LmsStudentManagementApplication.java
@@ -0,0 +1,13 @@
+package com.jm.lms.studentms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsStudentManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LmsStudentManagementApplication.class, args);
+    }
+
+}
