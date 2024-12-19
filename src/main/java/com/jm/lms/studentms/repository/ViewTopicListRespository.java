6b27db774fe06f1e045faca1c696b35727089766
BALA-KRISHNA-BALU <165011570+BALA-KRISHNA-BALU@users.noreply.github.com>
2024-12-19 09:54:17 +0530

    Update ViewTopicListRespository.java

--- src/main/java/com/jm/lms/studentms/repository/ViewTopicListRespository.java
+++ src/main/java/com/jm/lms/studentms/repository/ViewTopicListRespository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import com.jm.lms.studentms.model.TopicList;
 
 @Repository
-public interface TopicListRespository extends JpaRepository<TopicList,Long> {
+public interface ViewTopicListRespository extends JpaRepository<TopicList,Long> {
 	
 	Optional<TopicList> findByTopicTitle(String topicTitle);
 
