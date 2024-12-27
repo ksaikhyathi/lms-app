e382ee584b0fee3e4352eff5ff6bc4ed5f2faef7
PRASANNA677 <paturuprasanna578@gmail.com>
2024-12-27 10:36:05 +0530

    add Validations

--- src/main/java/com/jm/lms/studentms/controller/ViewTaskController.java
+++ src/main/java/com/jm/lms/studentms/controller/ViewTaskController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.jm.lms.studentms.model.TaskDetails;
 import com.jm.lms.studentms.service.ViewTaskService;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -28,7 +29,7 @@ public class ViewTaskController {
 	   }
 	   
 	   @GetMapping("/view")
-	    public List<TaskDetails> getAllTasks() {
+	    public List<TaskDetails> getAllTasks(@Valid TaskDetails taskDetails) {
 		   log.info("Entering getAllTasks method");
 	        List<TaskDetails> tasks = viewTaskService.getAllTasks();
 	        if (tasks.isEmpty()) {
